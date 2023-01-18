01a3a9e66fb742595ca8e29e06fe1310c8abba04
Desnecesauron <cesinhasampa63@gmail.com>
2023-01-18 00:00:06 -0300

    Commit project

---
+++ src/main/java/br/com/desnecesauron/nexumrpatest/NexumRpaTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.desnecesauron.nexumrpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexumRpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexumRpaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desnecesauron/nexumrpatest/NexumRpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desnecesauron.nexumrpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexumRpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
