ce7d922d18f7c2637c9b69f5c79f0decb4f7ea6c
Desnecesauron <cesinhasampa63@gmail.com>
2023-01-19 00:17:03 -0300

    Version improving Casas Bahia search (still in progress) and added postgreSQL docker and application.properties with db properties

--- src/main/java/br/com/desnecesauron/nexumrpatest/NexumRpaTestApplication.java
+++ src/main/java/br/com/desnecesauron/nexumrpatest/NexumRpaTestApplication.java
@@ -14,57 +14,113 @@ import java.util.List;
 @SpringBootApplication
 public class NexumRpaTestApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(NexumRpaTestApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(NexumRpaTestApplication.class, args);
 
-		System.setProperty("webdriver.chrome.driver", "/home/desnecesauron/Desktop/nexum-rpa-test/src/main/resources/chromedriver");
-		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--headless");
-		options.addArguments("--incognito");
-		WebDriver driver = new ChromeDriver(options);
+        System.setProperty("webdriver.chrome.driver", "/home/desnecesauron/Desktop/nexum-rpa-test/src/main/resources" +
+                "/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--headless");
+        options.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(options);
 
 
-		// ProductsGrid__ProductsGridWrapper-sc-yqpqna-0 ipfubY
-		//
-		// //*[@id="__next"]/div[2]/div/div/div[4]/div[2]/div/section/ul
-		// /html/body/div[1]/div[2]/div/div/div[4]/div[2]/div/section/ul
+        // ProductsGrid__ProductsGridWrapper-sc-yqpqna-0 ipfubY
+        //
+        // //*[@id="__next"]/div[2]/div/div/div[4]/div[2]/div/section/ul
+        // /html/body/div[1]/div[2]/div/div/div[4]/div[2]/div/section/ul
 
-		// casas bahia
-		driver.get("https://www.casasbahia.com.br/xbox-serie-s/b?sortby=relevance");
+        // casas bahia
+        driver.get("https://www.casasbahia.com.br/console-xbox-serie-s/b?sortby=relevance");
 
-		// div > div> div > div > div> div > div > section > ul
-//		WebElement webElement= driver.findElement(By.cssSelector("div > div> div > div > div> div > div > section > ul > li"));
-		List<WebElement> webElements= driver.findElements(By.cssSelector("div > div> div > div > div> div > div > section > ul > li"));
+        // div > div> div > div > div> div > div > section > ul
+//		WebElement webElement= driver.findElement(By.cssSelector("div > div> div > div > div> div > div > section > ul
+//		> li"));
+        String cssSelectorLiGeneralCasasBahia = "div > div > div > div > div > div > div > section > ul > li";
+        List<WebElement> webElements = driver.findElements(By.cssSelector(cssSelectorLiGeneralCasasBahia));
 
-		System.out.println("length");
-		System.out.println(webElements.toArray().length);
-		System.out.println("array");
-		System.out.println(Arrays.toString(webElements.toArray()));
-		int i = 0;
-		for (WebElement element : webElements) {
-			i++;
-			System.out.println("Volta " + i);
-			System.out.println(element.getText());
-		}
+        System.out.println("length");
+        System.out.println(webElements.toArray().length);
+        System.out.println("array");
+        System.out.println(Arrays.toString(webElements.toArray()));
 
-		// mercado livre
-		driver.get("https://lista.mercadolivre.com.br/xbox-serie-s");
-		// div > div > section > ol > li
-		List<WebElement> itensMercadoLivre= driver.findElements(By.cssSelector("div > div > section > ol > li"));
+        int i = 0;
+        for (WebElement element : webElements) {
+            i++;
+            System.out.println("Volta " + i);
 
-		System.out.println("length");
-		System.out.println(itensMercadoLivre.toArray().length);
-		System.out.println("array");
-		System.out.println(Arrays.toString(itensMercadoLivre.toArray()));
-		i = 0;
-		for (WebElement element : itensMercadoLivre) {
-			i++;
-			System.out.println("Volta " + i);
-			System.out.println(element.getText());
-		}
+            WebElement webElementLinkInItem = driver.findElement(By.cssSelector("div > div > div > div > div > div >" +
+                    " section > ul > li:nth-child(" + i + ") > div > div> a"));
+            WebElement webElementTitleInItem = driver.findElement(By.cssSelector("div > div > div > div > div > div >" +
+                    " section > ul > li:nth-child(" + i + ") > div > div> a  > h2"));
+            WebElement webElementPriceInItem = driver.findElement(By.cssSelector("div > div > div > div > div > div >" +
+                    " section > ul > li:nth-child(" + i + ") > div > div> a:nth-child(2) > div"));
+//			System.out.println(element.getText());
 
-		driver.close();
-	}
+            List<String> wordsUnnecessary = Arrays.asList("bateria", "suporte", "skin", "case", "capa", "charge",
+                    "serie x", "series x");
+            String titleCompare = webElementTitleInItem.getText().toLowerCase();
+            boolean isConsole = true;
+            if (!titleCompare.contains("xbox") &&
+                    (!titleCompare.contains("series s") || !titleCompare.contains("serie s"))) isConsole = false;
 
-	//https://www.casasbahia.com.br/xbox-serie-s/b?sortby=relevance
+            for (String doNotInclude : wordsUnnecessary) {
+                if (webElementTitleInItem.getText().toLowerCase().contains(doNotInclude))
+                    isConsole = false;
+            }
+            if (webElementPriceInItem.getText().toLowerCase().contains("indisponível")) {
+                continue;
+            }
+            if (!isConsole) continue;
+
+            System.out.println("Link: " + webElementLinkInItem.getAttribute("href"));
+            System.out.println("Title: " + webElementTitleInItem.getText());
+            System.out.println("Price: " + webElementPriceInItem.getText());
+
+        }
+
+        System.exit(0);
+
+        // mercado livre ------------------------------------------------------------------------------------
+        driver.get("https://lista.mercadolivre.com.br/xbox-serie-s");
+        // div > div > section > ol > li
+//        String cssSelectorLiGeneralMercadoLivre = "div > div > section > ol > li > div > div > a";
+        String cssSelectorLiGeneralMercadoLivre = "div > div > section > ol > li > div > div > a> div > div";
+        List<WebElement> itensMercadoLivre = driver.findElements(By.cssSelector(cssSelectorLiGeneralMercadoLivre));
+
+        System.out.println("length");
+        System.out.println(itensMercadoLivre.toArray().length);
+        System.out.println("array");
+        System.out.println(Arrays.toString(itensMercadoLivre.toArray()));
+        i = 0;
+        for (WebElement element : itensMercadoLivre) {
+            i++;
+            System.out.println("Volta " + i);
+            System.out.println(element.getText());
+         /*
+         List<WebElement> webElementsInItemMercadoLivre =
+                    driver.findElements(By.cssSelector(cssSelectorLiGeneralMercadoLivre + "div > div > section > ol >" +
+                            " li > div > div > a> div > div"));
+
+            for (WebElement elementItem : webElementsInItemMercadoLivre) {
+                System.out.println(elementItem.getText());
+            }
+            */
+
+			/*
+
+			int rowData = 0;
+            for (WebElement elementItem : webElementsInItem) {
+                if (rowData == 0 || rowData == 3 || rowData == 4)
+                    System.out.println(elementItem.getText());
+                rowData++;
+            }
+
+			*/
+        }
+
+        driver.close();
+    }
+
+    //https://www.casasbahia.com.br/xbox-serie-s/b?sortby=relevance
 }
